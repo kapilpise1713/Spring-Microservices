b70db8a890e59c62260c528c551a332f629eea8b
kapilpise1713 <kapilpise1713@gmail.com>
2024-10-26 18:45:33 +0530

    Server

---
+++ HealthCareManagement-ServerApp/src/test/java/com/cloud/hospital/HealthCareManagementServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareManagementServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
